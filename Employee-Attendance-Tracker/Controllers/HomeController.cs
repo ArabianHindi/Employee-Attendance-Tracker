7dac58b521baecd3f61e65e186d6b2ca628cf181
Kaizeus <31563719+Kaizeus@users.noreply.github.com>
2025-07-31 02:16:11 +0300

    Added the Department Views

--- Employee-Attendance-Tracker/Controllers/HomeController.cs
+++ Employee-Attendance-Tracker/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Employee_Attendance_Tracker.Models;
+using Employee_Attendance_Tracker.Models.ViewModels;
 
 namespace Employee_Attendance_Tracker.Controllers;
 
